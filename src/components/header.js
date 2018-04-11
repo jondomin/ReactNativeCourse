a5ca27bdbd5f45009952fd6a0fd3a0fcbbaf5f2e
Jonathan <jonathanldominguez@gmail.com>
2018-04-11 12:03:26 -0500

    Flow and Header Changes

--- src/components/header.js
+++ src/components/header.js
@@ -8,17 +8,26 @@ const styles = {
     alignItems: 'center',
     height: 60,
     paddingTop: 15,
+    shadowColor: '#000',
+    shadowOffset: { width: 0, height: 2 },
+    shadowOpacity: 0.2,
+    elevation: 2,
+    position: 'relative',
   },
   textStyle: {
     fontSize: 20,
   },
 };
 
-const Header = () => {
+type Props = {
+  headerText: "string"
+};
+
+const Header = (props: Props) => {
   const { textStyle, viewStyle } = styles;
   return (
     <View style={viewStyle}>
-      <Text style={textStyle}>Albums!</Text>
+      <Text style={textStyle}>{props.headerText}</Text>
     </View>
   );
 };
