237d1ef1b98417c1af8aab5d3022704b91eecfc7
Jonathan <jonathanldominguez@gmail.com>
2018-04-15 17:42:20 -0500

    Album Detail work

--- src/components/AlbumDetail.js
+++ src/components/AlbumDetail.js
@@ -1,18 +1,78 @@
 import React from 'react';
-import { Text } from 'react-native';
+import { Text, View, Image } from 'react-native';
 import Card from './Card';
 import CardSection from './CardSection';
+import Button from './Button';
 
-type Props = {
-    album: any,
+const styles = {
+  headerContentStlye: {
+    flexDirection: 'column',
+    justifyContent: 'space-around',
+  },
+  headerTextStyle: {
+    fontSize: 18,
+  },
+  thumbnailStyle: {
+    height: 50,
+    width: 50,
+  },
+  thumbnailContainerStyle: {
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginLeft: 10,
+    marginRight: 10,
+  },
+  imageStyle: {
+    height: 300,
+    flex: 1,
+    width: null,
+  },
+};
+
+type Album = {
+  title: string,
+  artist: string,
+  thumbnail_image: string,
+  image:string,
 }
-const AlbumDetail = (props: Props) => (
-  <Card>
-    <CardSection>
-      <Text>{props.album.title}</Text>
-    </CardSection>
-  </Card>
-);
+type Props = {
+  album: Album,
+};
+
+const AlbumDetail = (props: Props) => {
+  const {
+    title,
+    artist,
+    thumbnailImage = props.album.thumbnail_image,
+    image,
+  } = props.album;
+  const {
+    thumbnailStyle,
+    headerContentStlye,
+    thumbnailContainerStyle,
+    headerTextStyle,
+    imageStyle,
+  } = styles;
+  return (
+    <Card>
+      <CardSection>
+        <View style={thumbnailContainerStyle}>
+          <Image style={thumbnailStyle} source={{ uri: thumbnailImage }} />
+        </View>
+        <View style={headerContentStlye}>
+          <Text style={headerTextStyle}>{title}</Text>
+          <Text>{artist}</Text>
+        </View>
+      </CardSection>
+      <CardSection>
+        <Image style={imageStyle} source={{ uri: image }} />
+      </CardSection>
+      <CardSection>
+        <Button />
+      </CardSection>
+    </Card>
+  );
+};
 
 export default AlbumDetail;
 
